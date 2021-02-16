d91ca2382a8adadcade3a54bfc96ca6eeaa7aaf9
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-02-16 18:59:46 +0200

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * puts2 - function that prints every other character
+ *
+ * @str: pointer to char
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(*(str + i));
+		}
+		i += 1;
+	}
+	_putchar('\n');
+}
