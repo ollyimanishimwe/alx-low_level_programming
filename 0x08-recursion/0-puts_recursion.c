e22869abd6e4ad90ebdc5add7ba1abb6b892652b
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-02-24 21:12:03 +0200

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion -  function that prints a string
+ *
+ * @s: string to receive
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
