12635f07bb0f0e4bf354070ef1d5323abde04791
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-03-24 00:00:58 +0200

    100-main

--- 0x13-more_singly_linked_lists/100-main.c
+++ 0x13-more_singly_linked_lists/100-main.c
@@ -10,20 +10,20 @@
  */
 int main(void)
 {
-	listint_t *head;
+    listint_t *head;
 
-	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
-	reverse_listint(&head);
-	print_listint(head);
-	free_listint2(&head);
-	return (0);
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+    add_nodeint_end(&head, 4);
+    add_nodeint_end(&head, 98);
+    add_nodeint_end(&head, 402);
+    add_nodeint_end(&head, 1024);
+    print_listint(head);
+    reverse_listint(&head);
+    print_listint(head);    
+    free_listint2(&head);
+    return (0);
 }
