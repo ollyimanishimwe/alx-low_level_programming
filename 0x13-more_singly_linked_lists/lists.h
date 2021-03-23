cfdb5c6cdcffa2c83b9d7edae95bac3a68ae102f
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-03-24 00:15:35 +0200

    new modified changes

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,8 @@
 #ifndef _LISTS_H
 #define _LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,6 +16,8 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -24,4 +29,9 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
-#endif /* #ifndef _LISTS_H */
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif /* LISTS_H */
