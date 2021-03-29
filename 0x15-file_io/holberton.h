f582e8804564c7b95526c8db0940f452fb10805a
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-03-29 21:02:11 +0200

    holberton header

---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,6 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+#endif /* #ifndef _HOLBERTON_H */
