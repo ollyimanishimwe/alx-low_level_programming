f5675cff47e86f4c6b3d820a7ea580e94f6c4fe8
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-21 11:29:09 +0200

    101-make_me_win added

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/win.so https://github.com/ollyimanishimwe/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
-export LD_PRELOAD=/tmp/win.so
+wget -P /tmp https://github.com/ollyimanishimwe/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
+export LD_PRELOAD=/tmp/putshack.so
