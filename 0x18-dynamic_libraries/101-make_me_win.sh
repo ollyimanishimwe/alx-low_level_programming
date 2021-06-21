dac712c3ccd0e023c72e65a196494e93bac867bd
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-21 12:36:52 +0200

    101-make_me_win corrected

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ollyimanishimwe/alx-low_level_programming/blob/main/0x18-dynamic_libraries/101-make_me_win.sh
+wget -P /tmp https://github.com/ollyimanishimwe/alx-low_level_programming/raw/main/0x18-dynamic_libraries/putshack.so
 export LD_PRELOAD=/tmp/putshack.so
