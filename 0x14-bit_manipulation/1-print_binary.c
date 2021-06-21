fb379188294d829d63a7427c05f5e4d80ba4622e
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-21 09:50:38 +0200

    1-print_binary corrected

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,26 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "holberton.h"
-
+#include <math.h>
 /**
- * _pow_recursion - function that returns the value of x
- * raised to the power of y
- * @x: base number
- * @y: pow number
- * Return: int
+ * print_binary - Function that prints the binary representation of a number.
+ * Prototype: void print_binary(unsigned long int n);
+ * @n: number to convert to binary
+ * You are not allowed to use arrays
+ * You are not allowed to use malloc
+ * You are not allowed to use the % or / operators
  */
-
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	return (x * _pow_recursion(x, y - 1));
-}
-
-/**
- * print_binary - function that prints the binary representation of a number
- * @n: decimal number
- * Return: nothing
- */
-
 void print_binary(unsigned long int n)
 {
-	unsigned int res_pow = 0;
-	int exp = 10;
-	int flag = 0;
-
 	if (n == 0)
-		_putchar('0');
-	while (exp >= 0)
 	{
-		res_pow = _pow_recursion(2, exp);
-		if (n >= res_pow)
-		{
-			_putchar('1');
-			n -= res_pow;
-			flag = 1;
-		}
-		else if (n < res_pow && flag == 1)
-			_putchar('0');
-		exp--;
+		putchar('0');
+		return;
 	}
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
