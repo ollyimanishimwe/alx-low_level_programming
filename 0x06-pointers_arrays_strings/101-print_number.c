1414ee095955b56fed8bccc9b0351a334525817e
Olly Imanishimwe <imanishimweolyy@gmail.com>
2021-06-27 19:14:32 +0200

    101-print_number added

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,38 +1,23 @@
 #include "holberton.h"
-#include <stdlib.h>
+
 /**
- * print_number - check the code for Holberton School students.
- * @n: is a int number
- * Return: Always 0.
+ * print_number - Print the given int one digit at a time
+ * @n: The integer to print
  */
 void print_number(int n)
 {
-int cont = 1, num = n;
-
-if (n < 0)
-_putchar('-');
-
-while (num / 10 != 0)
-{
-cont = cont * 10;
-num = num / 10;
-}
-
-while ((n / cont != 0) && (cont != 1))
-{
-_putchar(abs((n / cont)) + '0');
-
-n = n % cont;
-
-if (cont > 1)
-cont = cont / 10;
-
-while ((abs(n) < cont) &&(cont > 1))
-{
-_putchar('0');
-cont = cont / 10;
-}
-}
-
-_putchar(abs(n) + '0');
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n > 0)
+		n *= -1;
+	else
+		_putchar('-');
+	if ((n / 10) != 0)
+	{
+		print_number((n / 10) * -1);
+	}
+	_putchar((n % 10) * -1 + '0');
 }
